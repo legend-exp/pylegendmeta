6fa308deebe620078eb102a8ae5a9eb5ef928f25
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-01-13 19:07:31 +0100

    Draft `LegendSlowControlDB.status()`
    Additionally:
    
    * add `connect` argument to `.__init__()`
    * add warning to `.make_session()`
    * try to automatically roll back the SQL transaction if any failure
      occurr

--- src/legendmeta/slowcontrol.py
+++ src/legendmeta/slowcontrol.py
@@ -1,5 +1,21 @@
+# Copyright (C) 2022 Luigi Pertoldi <gipert@pm.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 
+import logging
 import os
 from dataclasses import dataclass
 from datetime import datetime
@@ -7,6 +23,10 @@ from datetime import datetime
 import pandas
 import sqlalchemy as db
 from sqlalchemy.orm import DeclarativeBase, Mapped
+
+from .core import AttrsDict, LegendMetadata
+
+log = logging.getLogger(__name__)
 
 
 class LegendSlowControlDB:
@@ -18,8 +38,12 @@ class LegendSlowControlDB:
     class is to simplify access to the database from Python.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, connect=False) -> None:
         self.connection: db.engine.base.Connection = None
+        self.session: db.orm.Session = None
+
+        if connect:
+            self.connect()
 
     def connect(
         self, host: str = "localhost", port: int = 5432, password: str | None = None
@@ -94,6 +118,13 @@ class LegendSlowControlDB:
         --------
         `SQLAlchemy documentation <https://www.sqlalchemy.org/>`_
         """
+        if self.session:
+            log.warning(
+                "A session seems to be already open and available "
+                "at the .session attribute of this class object. "
+                "You might want to use that one."
+            )
+
         return db.orm.Session(self.connection)
 
     def dataframe(self, expr: str | db.sql.Select) -> pandas.DataFrame:
@@ -115,7 +146,7 @@ class LegendSlowControlDB:
         1        1  3899.4
         2        5  1120.2
 
-        >>> scdb.dataframe("diode_conf")
+        >>> scdb.dataframe("diode_conf_mon")
               confid  crate  slot  channel    vset  iset  rup  rdown  trip  vmax pwkill pwon                    tstamp
         0         15      0     0        0  4000.0   6.0   10      5  10.0  6000   KILL  Dis 2022-10-07 13:49:56+00:00
         1         15      0     0        1  4300.0   6.0   10      5  10.0  6000   KILL  Dis 2022-10-07 13:49:56+00:00
@@ -137,23 +168,127 @@ class LegendSlowControlDB:
         pandas.read_sql
         """
         try:
-            return pandas.read_sql(expr, self.connection)
-        except db.exc.ObjectNotExecutableError:
-            return pandas.read_sql(db.text(expr), self.connection)
-        # TODO: automatically rollback if failed transaction
-        # except db.exc.ProgrammingError as e:
-        #     self.connection.rollback()
-        #     raise e
+            try:
+                return pandas.read_sql(expr, self.connection)
+            except db.exc.ObjectNotExecutableError:
+                return pandas.read_sql(db.text(expr), self.connection)
+        # try rolling back transaction if any exception occurs
+        except Exception:
+            self.connection.rollback()
+            raise
 
-    def status(self, channel: dict, at: str | datetime, system: str = "ged") -> dict:
-        """Query information about a channel.
+    def status(self, channel: dict, on: str | datetime, system: str = None) -> dict:
+        """Query status of a LEGEND DAQ channel.
 
-        >>> channel = lmeta.hardware.configuration.channelmaps.on(ts).B00089B
-        >>> scdb.status(channel, at=ts)
+        Collect all the relevant information about the status of a channel at a
+        certain time from the Slow Control database, based on the channel type
+        (i.e. germanium detector, SiPM or PMT).
+
+        Parameters
+        ----------
+        channel
+            this dictionary must contain information about the channel hardware
+            configuration. Typically a LEGEND channel map entry (obtainable, for
+            example, with :meth:`.core.LegendMetadata.channelmap`).
+        on
+            time at which the status is requested.
+        system: "geds", "spms", "muon"
+            system the channel belong to. this information is used to ask the
+            Slow Control database the right questions. If ``None`` will try to
+            determine it from the available metadata.
+
+        Examples
+        --------
+        >>> ts = datetime(...) # or LEGEND cycle timestamp
+        >>> my_bege = lmeta.channelmap(on=ts).B00089B
+        >>> scdb.status(my_bege, on=ts)
+        {'vmon': 3399.9,
+         'imon': 0.00015,
+         'status': 1,
+         'vset': 3400.0,
+         'iset': 6.0,
+         ...
+
+        Warning
+        -------
+        This class method assumes a certain structure for legend-metadata.
+        Might stop working if that structure is altered.
         """
-        raise NotImplementedError
-        # df = self.dataframe(...tables...).sort_values(by=["tstamp"])
-        # return df.loc(df.tstamp <= at).iloc(-1)
+        # sanity checks
+        if isinstance(on, str):
+            on = datetime.strptime(on, "%Y%m%dT%H%M%SZ")
+        elif not isinstance(on, datetime):
+            raise ValueError("Bad input timestamp format")
+
+        if not isinstance(channel, dict):
+            raise ValueError("Bad channel format: dict expected")
+
+        if not system:
+            # now try to guess system
+            if (
+                "det_type" in channel
+                and isinstance(channel.det_type, str)
+                and channel.det_type
+            ):
+                dtype = channel.det_type
+            else:
+                lmeta = LegendMetadata()
+
+                # find channel info in detector database
+                detdb = lmeta.hardware.detectors
+                fulldb = detdb.germanium.detectors | detdb.lar.sipms
+
+                if "detname" not in channel:
+                    raise ValueError(
+                        "No 'detname' key in channel info: cannot deduce detector type"
+                    )
+                elif channel.detname not in fulldb:
+                    raise RuntimeError(
+                        f"Could not find '{channel.detname}' in detector database: cannot deduce detector type"
+                    )
+                else:
+                    dtype = fulldb[channel.detname].det_type
+
+            if dtype in ["bege", "icpc", "coax", "ppc"]:
+                system = "geds"
+            elif dtype == "sipm":
+                system = "spms"
+            else:
+                raise NotImplementedError(
+                    f"Detector type = {dtype}: cannot deduce system"
+                )
+
+        if not self.session:
+            self.session = self.make_session()
+
+        output = AttrsDict()
+        if system == "geds":
+            for tbl in [DiodeSnap, DiodeConfMon]:
+                stmt = (
+                    db.select(tbl)
+                    .where(tbl.slot == channel.daq.card.id)
+                    .where(tbl.channel == channel.daq.channel)
+                    .order_by(tbl.tstamp.desc())
+                    .where(tbl.tstamp <= on)
+                    .limit(1)
+                )
+
+                result = self.session.execute(stmt).first()
+
+                if not result:
+                    log.warning(f"Query on table '{tbl}' did not produce any result")
+                    continue
+
+                output |= result[0].asdict()
+
+        elif system == "spms":
+            pass
+        elif system == "muon":
+            pass
+        else:
+            raise NotImplementedError(f"System '{system}' not supported")
+
+        return output
 
 
 class Base(DeclarativeBase):
@@ -175,12 +310,15 @@ class DiodeSnap(Base):
     almask: Mapped[int]
     tstamp: Mapped[datetime] = db.orm.mapped_column(primary_key=True)
 
+    def asdict(self) -> AttrsDict:
+        return AttrsDict({"vmon": self.vmon, "imon": self.imon, "status": self.status})
+
 
 @dataclass
-class DiodeConf(Base):
+class DiodeConfMon(Base):
     """Configuration parameters of HPGe detectors."""
 
-    __tablename__ = "diode_conf"
+    __tablename__ = "diode_conf_mon"
 
     confid: Mapped[int]
     crate: Mapped[int]
@@ -195,6 +333,20 @@ class DiodeConf(Base):
     pwkill: Mapped[str]
     pwon: Mapped[str]
     tstamp: Mapped[datetime] = db.orm.mapped_column(primary_key=True)
+
+    def asdict(self) -> AttrsDict:
+        return AttrsDict(
+            {
+                "vset": self.vset,
+                "iset": self.iset,
+                "rup": self.rup,
+                "rdown": self.rdown,
+                "trip": self.trip,
+                "vmax": self.vmax,
+                "pwkill": self.pwkill,
+                "pwon": self.pwon,
+            }
+        )
 
 
 @dataclass
