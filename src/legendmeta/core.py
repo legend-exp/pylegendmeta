dc85fe08fde797c2d1f106e7b32caf2e8aaaa158
Ian Guinn <guinnis@ornl.gov>
2025-12-08 15:44:49 -0500

    Automatically add repo to safe directories list in git user config (#107)
    * Automatically add repo to safe directories list in git user config
    
    * Add warning message

--- src/legendmeta/core.py
+++ src/legendmeta/core.py
@@ -278,3 +278,12 @@ class LegendMetadata(TextDB):
         if self.__repo__ is None:
             msg = f"{self.__repo_path__} is not a Git repository (.git folder missing)"
             raise InvalidGitRepositoryError(msg)
+
+        try:
+            self.__repo__.git.rev_parse("HEAD")
+        except GitCommandError as e:
+            if "fatal: detected dubious ownership" in e.stderr:
+                msg = f"git raised dubious ownership error. Automatically called 'git config --global --add safe.directory {self.__repo_path__}`"
+                log.warning(msg)
+                with self.__repo__.config_writer("global") as c:
+                    c.add_value("safe", "directory", str(self.__repo_path__))
