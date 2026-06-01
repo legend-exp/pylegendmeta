7cb765f08174ac86e99bad50a33827d576732171
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2026-06-01 21:20:22 +0200

    fix: test_to_datetime compat with tz-aware dbetto str_to_datetime

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -6,4 +6,5 @@ from legendmeta import to_datetime
 
 
 def test_to_datetime():
-    assert to_datetime("20230501T205951Z") == datetime(2023, 5, 1, 20, 59, 51)
+    result = to_datetime("20230501T205951Z")
+    assert result.replace(tzinfo=None) == datetime(2023, 5, 1, 20, 59, 51)
